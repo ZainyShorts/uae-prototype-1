d4c623d81f1b41d4b50cc82752c4e7ca8cb04e27
ZainyShorts <ZainyShorts@gmail.com>
2025-02-01 10:19:14 +0500

    bug fix

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -2,9 +2,9 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { useColorScheme } from '@mui/material/styles';
+// import { useColorScheme } from '@mui/material/styles';
 
-import { NoSsr } from '@/components/core/no-ssr';
+// import { NoSsr } from '@/components/core/no-ssr';
 
 const HEIGHT = 60;
 const WIDTH = 60;
@@ -39,18 +39,19 @@ export interface DynamicLogoProps {
 }
 
 export function DynamicLogo({
-  colorDark = 'light',
-  colorLight = 'dark',
-  height = HEIGHT,
-  width = WIDTH,
-  ...props
+  // colorDark = 'light',
+  // colorLight = 'dark',
+  // height = HEIGHT,
+  // width = WIDTH,
+  // ...props
 }: DynamicLogoProps): React.JSX.Element {
-  const { colorScheme } = useColorScheme();
-  const color = colorScheme === 'dark' ? colorDark : colorLight;
+  // const { colorScheme } = useColorScheme();
+  // const color = colorScheme === 'dark' ? colorDark : colorLight;
 
   return (
-    <NoSsr fallback={<Box sx={{ height: `${height}px`, width: `${width}px` }} />}>
-      {/* <Logo color={color} height={height} width={width} {...props} /> */}
-    </NoSsr>
+    <></>
+    // <NoSsr fallback={<Box sx={{ height: `${height}px`, width: `${width}px` }} />}>
+    //   {/* <Logo color={color} height={height} width={width} {...props} /> */}
+    // </NoSsr>
   );
 }
